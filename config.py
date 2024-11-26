dfa35277eb8df296035dd69b0680b397a36c667c
Alan Breno Correa <alanbrenocorrea@gmail.com>
2024-11-26 20:36:29 -0300

    train_efficientnetb1_CCAD

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # Encoders disponíveis no Segmentation Models Pytorch
 ENCODER_NAME_EFFICIENTNETB0 = "timm-efficientnet-b0"
-ENCODER_NAME_EFFICIENTNETB2 = "timm-efficientnet-b1"
-ENCODER_NAME_EFFICIENTNETB3 = "timm-efficientnet-b2"
+ENCODER_NAME_EFFICIENTNETB1 = "timm-efficientnet-b1"
+ENCODER_NAME_EFFICIENTNETB2 = "timm-efficientnet-b2"
 
 # Nomes das pastas dos modelos
 NAME_EFFICIENTNETB0 = "Unet_timm-efficientnet-b0"
+NAME_EFFICIENTNETB1 = "Unet_timm-efficientnet-b1"
 NAME_EFFICIENTNETB2 = "Unet_timm-efficientnet-b2"
-NAME_EFFICIENTNETB3 = "Unet_timm-efficientnet-b3"
 
 # Hiperparâmetros do modelo
 LEARNING_RATE = 1e-3
 EPOCHS = 100
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 CLASSES = 4
 IN_CHANNELS = 13
 ACCELERATOR = "auto"
@@ -22,5 +22,5 @@ DIR_LOG = "/home/mseruffo/Unet/lightning_logs/"
 
 # Diretórios raiz dos modelos
 DIR_ROOT_EFFICIENTNETB0 = "/home/mseruffo/Unet/lightning_logs/Unet_timm-efficientnet-b0"
+DIR_ROOT_EFFICIENTNETB1 = "/home/mseruffo/Unet/lightning_logs/Unet_timm-efficientnet-b1"
 DIR_ROOT_EFFICIENTNETB2 = "/home/mseruffo/Unet/lightning_logs/Unet_timm-efficientnet-b2"
-DIR_ROOT_EFFICIENTNETB3 = "/home/mseruffo/Unet/lightning_logs/Unet_timm-efficientnet-b3"
