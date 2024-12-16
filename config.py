8e9876867ba42851aa8d5012c5756209602be814
Alan Breno Correa <alanbrenocorrea@gmail.com>
2024-12-16 18:10:37 -0300

    teste mobileones1

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 ENCODER_NAME_EFFICIENTNETB0 = "timm-efficientnet-b0"
 ENCODER_NAME_EFFICIENTNETB1 = "timm-efficientnet-b1"
 ENCODER_NAME_MOBILENETV2 = "mobilenet_v2"
-ENCODER_NAME_MOBILEONE = "mobileone_s0"
+ENCODER_NAME_MOBILEONE = "mobileone_s1"
 
 # Nomes das pastas dos modelos
 NAME_EFFICIENTNETB0 = "Unet_timm-efficientnet-b0"
 NAME_EFFICIENTNETB1 = "Unet_timm-efficientnet-b1"
 NAME_MOBILENETV2 = "Unet_mobilenet_v2"
-NAME_MOBILEONE = "Unet_mobileone_s0"
+NAME_MOBILEONE = "Unet_mobileone_s1"
 
 # Hiperparâmetros do modelo
 LEARNING_RATE = 1e-3
@@ -26,4 +26,4 @@ DIR_LOG = "/home/mseruffo/Unet_4bands/lightning_logs/"
 DIR_ROOT_EFFICIENTNETB0 = "/home/mseruffo/Unet_4bands/lightning_logs/Unet_timm-efficientnet-b0"
 DIR_ROOT_EFFICIENTNETB1 = "/home/mseruffo/Unet_4bands/lightning_logs/Unet_timm-efficientnet-b1"
 DIR_ROOT_MOBILENETV2 = "/home/mseruffo/Unet_4bands/lightning_logs/Unet_mobilenet_v2"
-DIR_ROOT_MOBILEONE = "/home/mseruffo/Unet_4bands/lightning_logs/Unet_mobileone_s0"
+DIR_ROOT_MOBILEONE = "/home/mseruffo/Unet_4bands/lightning_logs/Unet_mobileone_s1"
